21bf2ee5f95b91f4072c27587172620ce313407a
JuHaoChen1997 <juhaochen97@gmail.com>
2022-08-16 12:39:49 -0400

    add create snacks table in schema.sql

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,21 @@
+-- step 1 incase there is a database already, drop it
 DROP DATABASE IF EXISTS snack_a_log;
+
+--step 2 create the db
 CREATE DATABASE snack_a_log; 
 
+--step 3 connect to the db
 \c snack_a_log; 
+
+--step 4 create the songs table
+CREATE TABLE snacks (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    fiber INT NOT NULL,
+    protein INT NOT NULL,
+    added_sugar INT NOT NULL,
+    is_healthy BOOLEAN,
+    image TEXT
+)
 
 
