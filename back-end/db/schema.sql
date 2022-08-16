201f3da60f1d78f085b27503783856a24e1a9e89
named.josie <named.josie@gmail.com>
2022-08-16 14:22:44 -0400

    created table

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,20 @@
 DROP DATABASE IF EXISTS snack_a_log;
-CREATE DATABASE snack_a_log; 
 
+-- Creating the database 
+CREATE DATABASE snack_a_log;
+
+-- Connect 
 \c snack_a_log; 
 
+-- Create table
+CREATE TABLE 
+snacks (
+    id serial PRIMARY KEY,
+    name TEXT NOT NULL,
+    fiber INT NOT NULL,
+    protein INT NOT NULL,
+    added_sugar INT NOT NULL,
+    is_healthy BOOLEAN,
+    image  TEXT NOT NULL
 
+);
