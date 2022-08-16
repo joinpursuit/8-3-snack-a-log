0a222247d65c0a40fbfe4f3072d00b2a9335d92d
EmaleeSoto <emaleesoto@gmail.com>
2022-08-16 13:27:53 -0400

    Created Show and Create Routes

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { all } = require("../app");
 const snacks = express.Router();
-const { getAllSnacks } = require("../queries/snacks.js");
+const { getAllSnacks, getSnack, createSnack } = require("../queries/snacks.js");
+//TODO: Create functions to check validity of params
 
 // INDEX
 snacks.get("/", async (req, res) => {
@@ -10,6 +10,27 @@ snacks.get("/", async (req, res) => {
     res.status(200).json(allSnacks);
   } else {
     res.status(500).json({ error: "server error" });
+  }
+});
+
+// SHOW
+snacks.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const snack = await getSnack(id);
+  if (snack) {
+    res.json(snack);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// CREATE
+snacks.post("/new", async (req, res) => {
+  try {
+    const snack = await createSnack(req.body);
+    res.json(snack);
+  } catch (error) {
+    res.status(400).json({ error: error });
   }
 });
 
--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -8,4 +8,30 @@ const getAllSnacks = async () => {
   }
 };
 
-module.exports = { getAllSnacks };
+const getSnack = async (id) => {
+  try {
+    return await db.one("SELECT * FROM snacks WHERE id=$1", id);
+  } catch (error) {
+    return error;
+  }
+};
+
+const createSnack = async ({
+  name,
+  fiber,
+  protein,
+  added_sugar,
+  is_healthy,
+  image,
+}) => {
+  try {
+    return await db.one(
+      "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [name, fiber, protein, added_sugar, is_healthy, image]
+    );
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllSnacks, getSnack, createSnack };
