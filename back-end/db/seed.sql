f2b0533131d64e303ebec7ed5cc2d0015ba7b0d7
Isaac Gonzalez <u1337x@gmail.com>
2022-08-16 12:48:26 -0400

    some snacks

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -6,7 +6,10 @@ INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES
 ('Honey Covered Granola',  30, 12, 22, false, 'https://picsum.photos/id/312/300/300'),
 ('New Wave Nuts', 11, 55, 9, true, 'https://picsum.photos/id/139/300/300'),
 ('Raw Onions & Turnips', 11, 9, 9, true, 'https://picsum.photos/id/292/300/300'),
-('Healthy Birthday Cake Square', 4, 8, 19, false, 'https://content.nutrisystem.com/images/products/alc/large/BirthdayCakeSquare_L.jpg');
-
+('Healthy Birthday Cake Square', 4, 8, 19, false, 'https://content.nutrisystem.com/images/products/alc/large/BirthdayCakeSquare_L.jpg'),
+(' Doritos - Cool Ranch Doritos' , 1, 2, 1, false, 'https://www.doritos.com/sites/doritos.com/files/2020-06/%28medium%29%20Cool%20Ranch%20XXL%20no%20background.png'),
+('Little debbie - Cosmic brownies', 1, 2, 24, false, 'https://e22d0640933e3c7f8c86-34aee0c49088be50e3ac6555f6c963fb.ssl.cf2.rackcdn.com/0024300041060_CL_ITEMMASTERSYNDIGO_default_large.png'), 
+('Jack Link - ORIGINAL TURKEY JERKY', 0, 12, 4, true, 'https://www.jacklinks.com/shop/media/catalog/product/1/0/10000023121_3.25oz_jl_tu_orig_jerk_hero_1.png' ),
+('Pillsbury - Spooky cookies', 0, 1, 9, false 'https://www.instacart.com/image-server/466x466/filters:fill(FFF,true):format(webp)/www.instacart.com/assets/domains/product-image/file/large_55b46c7c-0b64-4957-aa0b-183c4e9a122d.png'), ;
 -- use advanced image search to choose images that are square (aspect ratio)
 -- https://www.google.com/advanced_image_search
