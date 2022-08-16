64a8c98699fdb9964f1b853e04d86abe4851ebf0
Jonathan Scheiber <jon@scheiber.me>
2022-08-16 12:54:33 -0400

    add dependencies, config, middleware, and routes

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,24 @@
 // DEPENDENCIES
-
+const cors = require("cors");
 const express = require("express");
+const snackController = require("./controllers/snackController.js");
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use("/snacks", snackController);
 
 // ROUTES
+app.get("/", (req, res) => {
+  res.send("Get Snack'n at Snack-a-log!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("404: Not Found");
+});
 
 // EXPORT
 module.exports = app;
