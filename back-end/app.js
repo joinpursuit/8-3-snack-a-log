711780d77bc5e4e34bf919099d52a0857d82787c
EmaleeSoto <emaleesoto@gmail.com>
2022-08-16 12:37:27 -0400

    Added home route to backend

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,19 @@
 // DEPENDENCIES
 
 const express = require("express");
+const cors = require("cors");
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(express.json());
+app.use(cors());
 
 // ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Snack A' Log");
+});
 
 // EXPORT
 module.exports = app;
