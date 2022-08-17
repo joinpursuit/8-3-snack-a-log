38371f8c12648d57ada446a59a4c2c8d1a657d54
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-08-17 10:30:08 -0400

    preparing to deploy front end, clearing errors

--- front-end/src/Components/EditSnack.js
+++ front-end/src/Components/EditSnack.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
-import { useState, useEffect } from 'react'
-import { useParams, Link, useNavigate } from 'react-router-dom'
+// import axios from 'axios'
+// import { useState, useEffect } from 'react'
+// import { useParams, Link, useNavigate } from 'react-router-dom'
 
 const EditSnack = () => {
     return (
