8d0cfc1679f33edc1eff4febc0885b9f70ad5e90
Jonathan Scheiber <jon@scheiber.me>
2022-08-17 12:28:16 -0400

    add getsinglesnack functionality

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -4,11 +4,11 @@ CREATE DATABASE snack_a_log;
 \c snack_a_log; 
 
 CREATE TABLE snacks(
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    fiber INT DEFAULT 0,
-    protein INT DEFAULT 0,
+    id          SERIAL PRIMARY KEY,
+    name        TEXT NOT NULL,
+    fiber       INT DEFAULT 0,
+    protein     INT DEFAULT 0,
     added_sugar INT DEFAULT 0,
-    is_healthy BOOLEAN,
-    image TEXT
+    is_healthy  BOOLEAN,
+    image       TEXT
 );
