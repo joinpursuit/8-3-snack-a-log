c622200f867d37c5d43ac9ae07c62e4f9a577c4f
Edwin Codrington <edwincodrington@gmail.com>
2022-08-17 10:32:17 -0400

    initial db config

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,13 @@
 DROP DATABASE IF EXISTS snack_a_log;
-CREATE DATABASE snack_a_log; 
+CREATE DATABASE snack_a_log;
+\c snack_a_log;
 
-\c snack_a_log; 
-
-
+CREATE TABLE snacks (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    fiber INT,
+    protein INT,
+    added_sugar INT,
+    is_healthy BOOLEAN,
+    image TEXT NOT NULL
+)
