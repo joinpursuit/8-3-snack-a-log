a82687f866712e29192faf94ccd8bda8b0069daa
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-08-17 12:27:49 -0400

    updated controller and queries

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const snacks = express.Router(); //helps us be able to set routes
 const db = require("../db/dbConfig");
+const { getSnack } = require("../queries/snacks");
 
 //Index
 snacks.get("/", async (req, res) => {
   const allSnacks = await db.any("SELECT * FROM snacks");
-  res.json({ success: true, payload: allSnacks });
+  res.json({ payload: allSnacks });
+});
+
+// Show
+snacks.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const snack = await getSnack(id);
+  if (snack) {
+    res.json({ success: true, payload: snack });
+  } else {
+    res.status(404).json({ success: false, error: "not found" });
+  }
 });
 
 module.exports = snacks;
--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -1,12 +1,21 @@
 const db = require("../db/dbConfig.js");
 
 const getAllSnacks = async () => {
-    try {
-      const allSnacks = await db.any("SELECT * FROM snacks");
-      return snacks;
-    } catch (error) {
-      return error;
-    }
-  };
+  try {
+    const allSnacks = await db.any("SELECT * FROM snacks");
+    return allSnacks;
+  } catch (error) {
+    return error;
+  }
+};
 
-module.exports = {};
+const getSnack = async (id) => {
+  try {
+    const oneSnack = await db.one("SELECT * FROM snacks WHERE id=$1", id);
+    return oneSnack;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = { getAllSnacks, getSnack };
