b8e817bbf475e6e5452cad4036e3257cadfbbb2c
named.josie <named.josie@gmail.com>
2022-08-17 20:41:55 -0400

    added snack details

--- front-end/src/Components/Pages/Index.js
+++ front-end/src/Components/Pages/Index.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import Snack_Details from '../Snack_Details'
 
 export default function Index() {
   return (
-    <div>Index</div>
+    <div>
+      <h1>Index</h1>
+    <Snack_Details/>
+  </div>
   )
 }
