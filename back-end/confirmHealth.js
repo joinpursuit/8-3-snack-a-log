fad65c69103a8beba0d926e7b00b6a84130f0f55
Jonathan Scheiber <jon@scheiber.me>
2022-08-18 13:41:53 -0400

    update snack health check logic

--- back-end/confirmHealth.js
+++ back-end/confirmHealth.js
@@ -1,3 +1,10 @@
-const confirmHealth = (snack) => {};
+const confirmHealth = (snack) =>
+  typeof snack.protein !== "number" &&
+  typeof snack.fiber !== "number" &&
+  typeof snack.added_sugar !== "number"
+    ? null
+    : snack.fiber >= 5 && snack.added_sugar < 5
+    ? true
+    : snack.protein >= 5 && snack.added_sugar < 5;
 
 module.exports = confirmHealth;
