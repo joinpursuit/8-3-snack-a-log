dbc6f668c2b02eb413a1b06c892bee489c02a4e7
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-08-18 11:33:32 -0400

    merging

--- front-end/cypress/integration/new.test.js
+++ front-end/cypress/integration/new.test.js
@@ -6,16 +6,20 @@ describe("The New page", () => {
   before(() => {
     cy.visit(`${URL}/snacks/new`);
   });
+
   it("shows the header text", () => {
     cy.contains("Snacks");
     cy.contains("New");
   });
+
   describe("the form", () => {
     // pay attention the capitalization!
     // cypress needs accuracy to find elements on the DOM\
+
     it("has a form with correct labels and fields", () => {
       // for the form inputs we use label/input
       // in this first one its called "name"
+
       cy.get("label").contains("Name").should("have.attr", "for", "name");
       cy.get("#name").should("have.attr", "type", "text");
       cy.get("label").contains("Image").should("have.attr", "for", "image");
@@ -29,11 +33,13 @@ describe("The New page", () => {
         .should("have.attr", "for", "added_sugar");
       cy.get("#added_sugar").should("have.attr", "type", "number");
     });
+
     it("can create a snack and then redirects back to index page", () => {
       cy.get("#name").type("Potato Chips");
       cy.get("#image").type(
         "https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image"
       );
+
       cy.get("#fiber").type("3");
       cy.get("#protein").type("1");
       cy.get("#added_sugar").type("1");
