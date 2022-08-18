c4e57938a84fccfc7445a85dc807b2d261dad409
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-08-18 11:04:43 -0400

    installed sass and fined cypress test

--- front-end/cypress/integration/new.test.js
+++ front-end/cypress/integration/new.test.js
@@ -6,37 +6,29 @@ describe("The New page", () => {
   before(() => {
     cy.visit(`${URL}/snacks/new`);
   });
-
   it("shows the header text", () => {
     cy.contains("Snacks");
     cy.contains("New");
   });
-
   describe("the form", () => {
     // pay attention the capitalization!
     // cypress needs accuracy to find elements on the DOM\
-
     it("has a form with correct labels and fields", () => {
       // for the form inputs we use label/input
-      // in this first one its called 'name'
+      // in this first one its called "name"
       cy.get("label").contains("Name").should("have.attr", "for", "name");
       cy.get("#name").should("have.attr", "type", "text");
-
       cy.get("label").contains("Image").should("have.attr", "for", "image");
       cy.get("#image").should("have.attr", "type", "text");
-
       cy.get("label").contains("Fiber").should("have.attr", "for", "fiber");
       cy.get("#fiber").should("have.attr", "type", "number");
-
       cy.get("label").contains("Protein").should("have.attr", "for", "protein");
       cy.get("#protein").should("have.attr", "type", "number");
-
       cy.get("label")
         .contains("Added Sugar")
         .should("have.attr", "for", "added_sugar");
       cy.get("#added_sugar").should("have.attr", "type", "number");
     });
-
     it("can create a snack and then redirects back to index page", () => {
       cy.get("#name").type("Potato Chips");
       cy.get("#image").type(
@@ -51,5 +43,6 @@ describe("The New page", () => {
       cy.contains("Potato Chips").within(() => {
         cy.get("span img").should("have.attr", "alt", "unhealthy food");
       });
+    });
   });
 });
