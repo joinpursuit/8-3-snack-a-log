67ba30437a90475ba0441e20b29194e3dc1ca8ea
Lortiz528 <lortiztech@gmail.com>
2022-08-18 11:33:10 -0400

    front end files created

---
+++ front-end/src/Pages/Edit.js
@@ -0,0 +1,11 @@
+function Edit() {
+    return (
+      <div className="New Edit">
+        <h2>Edit snack here</h2>
+        <EditSnack />
+      </div>
+    );
+  }
+  
+  export default Edit;
+  
---
+++ front-end/src/Pages/New.js
@@ -0,0 +1,10 @@
+function New() {
+    return (
+      <div className="New">
+        <h2>Create new snack here</h2>
+        <NewSnack />
+      </div>
+    );
+  }
+  
+  export default New;
