c7d8c7a5ff3e5f7102ffcb97a3c8d641e57abf7d
Jonathan Scheiber <jon@scheiber.me>
2022-08-18 14:10:27 -0400

    add createsnack functionality

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -4,7 +4,11 @@ const {
   getAllSnacks,
   getSingleSnack,
   deleteSnack,
+  createSnack,
 } = require("../queries/snacks.js");
+
+const { stringFormatter } = require("../helpers/stringFormatter.js");
+const confirmHealth = require("../confirmHealth.js");
 
 // GET ALL SNACKS
 snacks.get("/", async (req, res) => {
@@ -44,6 +48,46 @@ snacks.delete("/:id", async (req, res) => {
   } else {
     res.status(404).json({ success: false, payload: { id: undefined } });
   }
+});
+
+// CREATE SNACK
+snacks.post("/", async (req, res) => {
+  const { body } = req;
+  body.is_healthy = confirmHealth(body);
+
+  const createdSnack = await createSnack(body);
+
+  if (createdSnack.id && !createdSnack.image) {
+    res.status(200).json({
+      success: true,
+      payload: {
+        id: createdSnack.id,
+        name: stringFormatter(createdSnack.name),
+        fiber: createdSnack.fiber,
+        protein: createdSnack.protein,
+        added_sugar: createdSnack.added_sugar,
+        is_healthy: createdSnack.is_healthy,
+        image: "https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image",
+      },
+    });
+    return;
+  }
+  if (createdSnack.id) {
+    res.status(200).json({
+      success: true,
+      payload: {
+        id: createdSnack.id,
+        name: stringFormatter(createdSnack.name),
+        fiber: createdSnack.fiber,
+        protein: createdSnack.protein,
+        added_sugar: createdSnack.added_sugar,
+        is_healthy: createdSnack.is_healthy,
+        image: createdSnack.image,
+      },
+    });
+    return;
+  }
+  res.status(500).json({ error: "Snack creation error" });
 });
 
 // EXPORT
--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -24,8 +24,27 @@ const deleteSnack = async (id) => {
   }
 };
 
+const createSnack = async (snack) => {
+  try {
+    return await db.one(
+      "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        snack.name,
+        snack.fiber,
+        snack.protein,
+        snack.added_sugar,
+        snack.is_healthy,
+        snack.image,
+      ]
+    );
+  } catch (error) {
+    return error;
+  }
+};
+
 module.exports = {
   getAllSnacks,
   getSingleSnack,
   deleteSnack,
+  createSnack,
 };
