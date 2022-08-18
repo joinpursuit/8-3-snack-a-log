1ec5d0a06960970a272073be6cce03e38c4a43c7
PratimaRoy <pratimaroy170@gmail.com>
2022-08-18 10:23:44 -0400

    Finished the backend

--- back-end/confirmHealth.js
+++ back-end/confirmHealth.js
@@ -1,3 +1,12 @@
-const confirmHealth = (snack) => {};
+const confirmHealth = (snack) => {
+    const {protein, fiber, added_sugar} = snack;
+    if(isNaN(fiber) || isNaN(protein) || isNaN(added_sugar)){
+        return null;
+    }else if((protein >= 5 || fiber >= 5) && added_sugar < 5){
+        return true;
+    }else{
+        return false;
+    }
+};
 
 module.exports = confirmHealth;
