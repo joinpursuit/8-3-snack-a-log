3c6c998b7a504d7d38cb9b9333f042b09f594138
meera-ramesh19 <meeraramesh19@outlook.com>
2022-08-18 08:03:29 -0400

    passed routes test

--- back-end/confirmHealth.js
+++ back-end/confirmHealth.js
@@ -1,3 +1,20 @@
-const confirmHealth = (snack) => {};
+const confirmHealth = (snack) => {
+	if (
+		typeof snack.fiber !== "number" &&
+		typeof snack.protein !== "number" &&
+		typeof snack.added_sugar !== "number"
+	) {
+		return null;
+	}
+
+	if (snack.fiber >= 5 && snack.added_sugar < 5) return true;
+
+	if (snack.protein > 5 && snack.added_sugar < 5) return true;
+
+	if (snack.protein > 5 && snack.added_sugar < 5 && snack.fiber > 5)
+		return true;
+
+	return false;
+};
 
 module.exports = confirmHealth;
