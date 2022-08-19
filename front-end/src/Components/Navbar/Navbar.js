65d09a5010463009b88eab2e4a58878f357ee2ea
Antonio Shivers <antonioshivers2500@yahoo.com>
2022-08-19 10:18:18 -0400

    changed style of header

--- front-end/src/Components/Navbar/Navbar.js
+++ front-end/src/Components/Navbar/Navbar.js
@@ -1,19 +1,26 @@
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import SnackImg from "../../images/snacksAttack.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <h1>
-        <Link to="/">Go home</Link>
-      </h1>
-      <h1>
+      <Link to="/">
+        <img src={SnackImg} alt="snacks attack" className="snackImg" />
+      </Link>
+
+      <h1 className="navSnackLink">
         <Link to="/snacks">snacks</Link>
       </h1>
-      <Button variant="outline-primary">
-        <Link to="/snacks/new">New Snack</Link>
-      </Button>
+
+      <h1>
+        <Button variant="outline-light" className="newSnackButton">
+          <Link to="/snacks/new" className="navNewSnack">
+            New Snack
+          </Link>
+        </Button>
+      </h1>
     </nav>
   );
 }
