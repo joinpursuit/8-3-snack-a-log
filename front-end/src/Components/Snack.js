c4696ab93d340d0f7fb73f82e32e8b2b8f10313b
meera-ramesh19 <meeraramesh19@outlook.com>
2022-08-19 02:07:02 -0400

    changed messages

--- front-end/src/Components/Snack.js
+++ front-end/src/Components/Snack.js
@@ -4,16 +4,19 @@ import { Link } from "react-router-dom";
 export default function Snack({ snack }) {
   return (
     <div className='snack-card'>
-      <img
-        style={{ width: '200px', height: '200px' }}
-        src={snack.image}
-        alt=''
-      />
-      <Link to={`{/snacks/${snack.id}}`} key={snack.id}>
+      
+      <Link to={`/snacks/`+snack.id} key={snack.id}>
+        <img
+          style={{ width: '200px', height: '200px' }}
+          src={snack.image}
+          alt=''
+        />
+         <p>{snack.name}</p>
+      </Link>
       <p>{snack.fiber}</p>
       <p>{snack.protein}</p>
       <HeartHealth snackHealth={snack} />
-    </Link>
+   
     </div>
   );
 }
