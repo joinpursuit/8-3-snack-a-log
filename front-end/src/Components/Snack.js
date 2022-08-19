4ce9a6455e58511c98cddb4139ee7437eda8faf0
PratimaRoy <pratimaroy170@gmail.com>
2022-08-19 10:30:21 -0400

    Some fix

--- front-end/src/Components/Snack.js
+++ front-end/src/Components/Snack.js
@@ -4,7 +4,7 @@ import HeartHealth from "./HeartHealth";
 function Snack({ snack }) {
   return (
     <div className="Snack">
-      <h4><img src={snack.image} alt={snack.name} /></h4>
+      <h4> <span><img src={snack.image} alt={snack.name}  /></span></h4>
       <Link to={`/snacks/${snack.id}`}>
         <h4>{snack.name}</h4>
         <h4><HeartHealth snackHealth={snack.is_healthy} /></h4>
