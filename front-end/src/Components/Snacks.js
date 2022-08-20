58e64c85849d4dfcf91d07eaecc554d22c3df9e0
Isaac Gonzalez <u1337x@gmail.com>
2022-08-20 17:47:54 -0400

    Index

--- front-end/src/Components/Snacks.js
+++ front-end/src/Components/Snacks.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Snack from "./Snack";
 
-const Snacks = () => {
+const API = process.env.REACT_APP_API_URL;
+
+export default function Snacks() {
+  const [snacks, setSnack] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${API}/snacks`)
+      .then((res) => {
+        setSnack(res.data.payload);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div>Snacks</div>
-  )
+    <div>
+      <h1>Snacks :D</h1>
+      {snacks.map((snack) => {
+        return <Snack key={snack.id} snack={snack} />;
+      })}
+    </div>
+  );
 }
-
-export default Snacks
