f36fa1b29b7f7e53d851d88694f937811c2d44ee
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-08-20 14:15:53 -0300

    all tests pass

--- back-end/confirmHealth.js
+++ back-end/confirmHealth.js
@@ -1,3 +1,18 @@
-const confirmHealth = (snack) => {};
+const confirmHealth = (snack) => {
+  const { fiber, added_sugar, protein } = snack;
+  if (protein === null || added_sugar === null || fiber === null) {
+    return null;
+  }
+
+  if (added_sugar <= 5) {
+    if (protein >= 5 || fiber >= 5) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
 
 module.exports = confirmHealth;
