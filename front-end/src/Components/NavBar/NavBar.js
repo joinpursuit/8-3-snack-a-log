28cfa7f599cb0f71bc00b1c2437dee21b48b61fc
Jonathan Scheiber <jon@scheiber.me>
2022-08-22 00:48:25 -0400

    update wordmark font and rollover color

--- front-end/src/Components/NavBar/NavBar.js
+++ front-end/src/Components/NavBar/NavBar.js
@@ -3,27 +3,27 @@ import './NavBar.css';
 
 const NavBar = () => {
 	return (
-		<nav className='nav'>
-		<h1>Snacks</h1>
-		
-            &nbsp;
-            
-            <Link className="links" to="/">Home</Link>
-        
-            &nbsp;
-
-            <Link className="links" to="/snacks">Snacks</Link>
-            &nbsp;
-
-            <Link className="links" to='/about'>About</Link>
-
-			<Link className='links' to='/snacks/new'>
-				<button className='nav-button'>New Snack</button>
-			</Link>
-
-            
-		</nav>
-	);
+    <nav className="nav">
+      <h1>
+        <a className="wordmark" href="/">Snacks</a>
+      </h1>
+      &nbsp;
+      <Link className="links" to="/">
+        Home
+      </Link>
+      &nbsp;
+      <Link className="links" to="/snacks">
+        Snacks
+      </Link>
+      &nbsp;
+      <Link className="links" to="/about">
+        About
+      </Link>
+      <Link className="links" to="/snacks/new">
+        <button className="nav-button">New Snack</button>
+      </Link>
+    </nav>
+  );
 };
 
 export default NavBar;
